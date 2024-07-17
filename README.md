acbcd89f48a6bb25f280692a289a4e8f8fcac002
ADBeta <ashley.adbeta@gmail.com>
2024-07-17 20:13:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CH32V003 High(ish) Current DC Motor PWM Driver
+
+
+----
+ADBeta (c) 2024
---
+++ Software/src/Motor_Driver.c
@@ -0,0 +1,13 @@
+#include "ch32v003fun.h"
+#include <stdio.h>
+
+
+int main()
+{
+	SystemInit();
+
+	while(1)
+	{
+	
+	}
+}
